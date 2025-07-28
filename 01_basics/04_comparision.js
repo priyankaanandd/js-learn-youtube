a03b6d1e247248a694266f317c55df00df558f67
priyankaanandd <anandpriyanka186@gmail.com>
2025-07-28 00:17:17 +0000

    comparison

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,19 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+ console.log("2" > 1);//t
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+ console.log("02" > 1);//t
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(null > 0);//f
+console.log(null == 0);//f
+console.log(null >= 0);//t
+//comparison op and equality check works diff comparisio conv null to a number so >=0 gives t and ==0 f
+
+console.log(undefined == 0);//f
+console.log(undefined > 0);//f
+console.log(undefined < 0);//f
 
 // === 
-
-console.log("2" === 2);
+console.log("2"==2)//t
+console.log("2" === 2);//f bcoz double ewual check that data type is same or not
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,19 +2,18 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const a=100
+const b=100.3
+let c; //here c is undefined 
+//both are in Number cateogry nothing special for decimal
+//so js is dynamically typed bcoz here not define sreing int for varibale
+const id=Symbol('123')
+const id2=Symbol('123')
+console.log(id)//Symbol(123)
+console.log(id==id2)//false bcoz even if values are same symbol gives uniques values
+const num=121224353455736n
+//last mei n lgane se datatype is bigInt
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
-
-const id = Symbol('123')
-const anotherId = Symbol('123')
-
-console.log(id === anotherId);
-
-// const bigNumber = 3456543576654356754n
 
 
 
@@ -27,11 +26,14 @@ let myObj = {
     name: "hitesh",
     age: 22,
 }
-
+//obj in curly braces
+//function(){} this is a func defining
 const myFunction = function(){
     console.log("Hello world");
 }
-
-console.log(typeof anotherId);
+console.log(typeof myFunction); //function
+console.log(typeof heros);//object
+console.log(typeof myObj);//object
+console.log(typeof id2);//symbol
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
