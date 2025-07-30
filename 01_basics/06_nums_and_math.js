f0b2f30cdffaa9c0521886c504f5ed36c801d6a3
priyankaanandd <anandpriyanka186@gmail.com>
2025-07-30 13:23:56 +0000

    math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,34 +1,35 @@
-const score = 400
-// console.log(score);
+/*const num=100;
+console.log(num);//100
 
-const balance = new Number(100)
-// console.log(balance);
+const nu=new Number(100);
+console.log(nu);//[Number: 100]
+console.log(nu.toString());//100
+//console.log(nu.toNumber());//func donot exists 
+console.log(nu.toFixed(6));//100.000000 precision 
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
 
-const otherNumber = 123.8966
+const a=23.64533;
+console.log(a.toPrecision(1));//precise krti h us lentth  tk value ko 
 
-// console.log(otherNumber.toPrecision(4));
+const b=1000000;
+console.log(b.toLocaleString('en-IN'));//10,00,000
+//enin is acc to india srtd and ye func comma lgata h bs 
 
-const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+//***********MATHS*********
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+console.log(Math);//object[Math]
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
+console.log(Math.abs(-4));//4 
+console.log(Math.round(2.333546464));//2 round of krta h
+console.log(Math.ceil(4.36446));//5
+console.log(Math.floor(4.3536));//4
+console.log(Math.min(4,2,5,2));*/
+//2 smlry there is max func also 
+console.log(Math.random());//hmesha 0 or 1 k beechmei value aaaega random//0.8....
+console.log(Math.random()*10+1);//7.3......
+//+1 bcoz value 0 bhe ho skta h toh *10 ka kuch significsnce nhi rhega 
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
-
-const min = 10
-const max = 20
-
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+//max-min ti specify range +1 to avoid ans 0 and +min to make it above min always 
