6759a87847475ad640d7566e4426b5b8acdec798
priyankaanandd <anandpriyanka186@gmail.com>
2025-07-30 15:06:01 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,39 +1,61 @@
 // array
+/*
+const arr=[1,2,4,24,"true",true]
+console.log(arr);
+console.log(arr[4]); //true 0based indexing h
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+//agar hm iski copy bnaege to v oshallow hogi deep nhi 
+//shallow copy vo hoti h mtlb usi orig arrpe refer hogaa mtlb usi mei chsnge hogaaa
 
-const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+//methods 
+const a=new Array("priya","riya")
+console.log(a.length);//2
+//NOTE array mei bhi prototype hote h
+a.push(6);
+a.push(true)
+console.log(a); //[priya,riya,6,true]
+a.pop() //remove last value of arr
+console.log(a)//[priya riya 6]
+a.unshift(true);
+console.log(a);//[true,priya,riya,6] ye value start mei add hofi
+a[0]=1;
+console.log(a);//[1,priya,riya,6]
+a.shift();
+console.log(a)//[priya ,riya,6] ab pehli value remove hogi hmesha 
 
-// Array methods
-
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
-
-// myArr.unshift(9)
-// myArr.shift()
-
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
-
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log( newArr);
-
-
-// slice, splice
-
-console.log("A ", myArr);
-
-const myn1 = myArr.slice(1, 3)
-
-console.log(myn1);
-console.log("B ", myArr);
+console.log(a.includes('riya'));//true
+console.log(a.indexOf(1))//-1 bcoz it doesnt ecist
+console.log(a.indexOf('priya'))//0
+*/
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// const a=[1,2,4,3]
+// const b=a.join()
+
+// console.log(a)//[1,2,4,3]
+// console.log(b)//1,2,4,3
+// console.log(typeof(b))//string
+// console.log(typeof(a))//object
+//so join hmare arr ko string bna deta h
+
+
+
+
+//slice and spice
+const arr=[1,2,3]
+console.log("A",arr) //2 [1,2,3]
+const b=arr.slice(1,2)//2 not include [2]
+console.log("B",b) //B [2]
+
+const c=arr.splice(1,3)
+console.log("C",c);
+
+//NOTE slice donot change the orig array but splice does change 
+//in slice end idx is not includes
+const ar = [1, 2, 3, 4, 5];
+const sliced = arr.slice(1, 4);  // [2, 3, 4]
+
+console.log(sliced); // [2, 3, 4]
+console.log(ar);    // [1, 2, 3, 4, 5] → original unchanged
+
+//in splice the element are remove form the mentioned index including the last idx
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,54 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+ //marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+ //console.log(marvel_heros);//[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+ //NOTE here push did not murge the array isne poore array ko ek single element ki tarah marvelheros mei add krdia 
+
+
+
+
+
+
+// console.log(marvel_heros[3][1]);//flash
+
+const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
+
+//note  push works on exisitng arr but cocatenate donot change orig array
+
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+//console.log(all_new_heros);//arrays merged to 1 now 
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+console.log(real_another_array);//[1, 2, 3, 4, 5, 6, 7, 6, 7, 4,5]
+
+
+const real_another_array1 = another_array.flat(1)
+//console.log(real_another_array1);//[1,2, 3, 4, 5, 6, 7, 6, 7,[4,5]]
+//flat complex array ko simle krta h till gove depth here infinity to poore ko krdia solbe 
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+
+
+//console.log(Array.isArray("Hitesh"))//false
+//console.log(Array.from("Hitesh"))//[ 'H', 'i', 't', 'e', 's', 'h' ]
+//console.log(Array.from("123"))//['1','2','3']
+//console.log(Array.from({name: "hitesh"})) //[]// interesting
+//here op is [] bcoz it didnt know ki keys ka array bnana h ya value ka 
+//from basically array mei conv krna h input ko
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+ console.log(Array.of(score1, score2, score3));//[100,200,300]
