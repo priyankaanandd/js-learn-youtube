85dd318da5910e4c3d65746cc454cc37e4711126
priyankaanandd <anandpriyanka186@gmail.com>
2025-07-30 12:49:03 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,40 @@
+//string can be donated by single or double quote
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); hitest50 value
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
-
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
+//another way of defining string this is string object
 const gameName = new String('hitesh-hc-com')
-
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(typeof gameName)//object
+// console.log(gameName[0]);bcoz key value paid
+ console.log(gameName.__proto__);//{}
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+//it has heap mem bcoz obj but here orig value does not change bcoz string are immutable 
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+//slicing 
+// const newString = gameName.substring(0, 4)//hite
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-2, -1);
 console.log(anotherString);
+//in slice we can give neq value and for neg it start from rev
+//in subs if we put neg it ignores and simply starts from 0
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);"   hitesh    "
+// console.log(newStringOne.trim());"hitesh" trims the start endin spaces
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+// const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-')) replace 20 in url with-
 
-console.log(url.includes('sundar'))
+// console.log(url.includes('sundar')) false
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-')); based on - jahan jahan - vahan se string split hojae
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -37,3 +37,21 @@ console.log(typeof myObj);//object
 console.log(typeof id2);//symbol
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//memoty 2 types 
+//1 stack mem used for prim types
+///in stack mem jo bhi var define hua uski copy milti h
+//2 heap mem in non prom type
+//in heap vahan se hme orig value ka ref milta h change in orig
+
+let name="priya"
+let name2=name //here copy of name is prov and not real name so changing name2 further dont change name
+name2="riya"
+console.log(name2)//riya
+let user={
+    nam:"priya",
+    age:21
+}
+let user2=user
+user2.name="ritika"
+console.log(user.name)//ritika bcoz heap memory and user and user2 have same reference 
